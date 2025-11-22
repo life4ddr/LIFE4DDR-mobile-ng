e1ab56d95112146ed0c3a4d88d5c9ebec8f52e52
Corey Perrigo <corey@perrigogames.com>
2025-11-22 13:18:11 -0800

    add client Ktor and the base HttpClient

---
+++ shared/src/commonMain/kotlin/com/perrigogames/life4ddr/nextgen/api/base/BaseHttpClient.kt
@@ -0,0 +1,39 @@
+package com.perrigogames.life4ddr.nextgen.api.base
+
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.http.ContentType
+import io.ktor.serialization.kotlinx.KotlinxSerializationConverter
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+// TODO Logger
+
+fun baseHttpClient(
+    json: Json,
+//    log: co.touchlab.kermit.Logger
+) = HttpClient {
+    install(ContentNegotiation) {
+        json(json)
+        register(ContentType.Text.Any, KotlinxSerializationConverter(json))
+    }
+    install(Logging) {
+        logger = object : Logger {
+            override fun log(message: String) {
+//                log.v { message }
+            }
+        }
+
+        level = LogLevel.INFO
+    }
+    install(HttpTimeout) {
+        val timeout = 30000L
+        connectTimeoutMillis = timeout
+        requestTimeoutMillis = timeout
+        socketTimeoutMillis = timeout
+    }
+}
