0bd3ba11f00d5f5e6d5127cfbd7438482d719bff
Corey Perrigo <corey@perrigogames.com>
2026-02-08 18:16:33 -0800

    Version 0.7.4

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -11,8 +11,8 @@ plugins {
     alias(libs.plugins.mokoResources)
 }
 
-private val VERSION_CODE = 15
-private val VERSION_NAME = "0.7.3"
+private val VERSION_CODE = 16
+private val VERSION_NAME = "0.7.4"
 
 kotlin {
     androidTarget {
