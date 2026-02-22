c70040464fd2788f438c852cacbad280c4e84baa
Corey Perrigo <corey@perrigogames.com>
2026-02-21 20:55:04 -0800

    implement ExternalActions

--- shared/src/iosMain/kotlin/com/perrigogames/life4ddr/nextgen/ExternalActions.ios.kt
+++ shared/src/iosMain/kotlin/com/perrigogames/life4ddr/nextgen/ExternalActions.ios.kt
@@ -1,14 +1,22 @@
 package com.perrigogames.life4ddr.nextgen
 
 import com.perrigogames.life4ddr.nextgen.util.ExternalActions
+import platform.Foundation.NSURL
+import platform.UIKit.UIApplication
 
 class IosExternalActions : ExternalActions {
-
     override fun openEmail(email: String) {
-        TODO("Not yet implemented")
+        NSURL.URLWithString("mailto:$email")?.let { openUrl(it) }
     }
 
     override fun openWeblink(url: String) {
-        TODO("Not yet implemented")
+        NSURL.URLWithString(url)?.let { openUrl(it) }
+    }
+
+    private fun openUrl(url: NSURL) {
+        val app = UIApplication.sharedApplication()
+        if (app.canOpenURL(url)) {
+            app.openURL(url)
+        }
     }
 }
